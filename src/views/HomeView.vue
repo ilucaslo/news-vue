368303aae99e1aa8a44f6fd034d97fcf2a2df6f4
Isma <ilucaslozano@gmail.com>
2022-12-22 14:24:54 +0100

    feat: basic grid system for the new cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,21 @@
 <template>
-  <new-item />
+  <v-container
+    class="grey lighten-5"
+    fluid>
+    <v-row>
+      <v-col
+        v-for="n in 12"
+        :key="n"
+        cols="12"
+        xs="1"
+        sm="4"
+        md="3"
+        lg="3"
+      >
+        <new-item />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
