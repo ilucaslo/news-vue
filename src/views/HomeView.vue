dd717f6f99f048208e9d3eb8b33808b626002e0e
Isma <ilucaslozano@gmail.com>
2022-12-22 14:46:23 +0100

    feat: subheader and filter button in the home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,23 @@
     class="grey lighten-5"
     fluid>
     <v-row>
+      <v-col>
+        <h2>Headlines</h2>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn
+        depressed
+        color="primary"
+        >
+          Filter
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
       <v-col
         v-for="n in 12"
         :key="n"
-        cols="12"
+        cols="auto"
         xs="1"
         sm="4"
         md="3"
