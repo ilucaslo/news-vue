4cc00ea29a6e5f19be70bb8651bf52a3e7069bbc
Isma <ilucaslozano@gmail.com>
2022-12-25 19:34:51 +0100

    feat(store): handling errors

--- src/store/index.js
+++ src/store/index.js
@@ -25,24 +25,43 @@ export default new Vuex.Store({
     clearHeadlinesHistory(state) {
       state.headlinesHistory = [];
     },
+    editArticleTitle(state, titleInfo) {
+      state.articles[titleInfo.articleIndex].title = titleInfo.title;
+    },
   },
   actions: {
     async fetchTopArticles({ commit }) {
-      const res = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=099148be22804e849a0c6fe022b7cf5e');
-      const data = await res.json();
-      commit('setArticles', data.articles);
+      const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.VUE_APP_NEWS_API_KEY}`);
+      if (!res.ok) {
+        this.dialog = true;
+        const message = `An error occured: ${res.status}`;
+        throw new Error(message);
+      } else {
+        const data = await res.json();
+        commit('setArticles', data.articles);
+      }
     },
     async fetchSources({ commit }) {
-      const res = await fetch('https://newsapi.org/v2/sources?apiKey=099148be22804e849a0c6fe022b7cf5e');
-      const data = await res.json();
-      commit('setSources', data.sources);
+      const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${process.env.VUE_APP_NEWS_API_KEY}`);
+      if (!res.ok) {
+        this.dialog = true;
+        const message = `An error occured: ${res.status}`;
+        throw new Error(message);
+      } else {
+        const data = await res.json();
+        commit('setSources', data.sources);
+      }
     },
     async searchArticles({ commit }, searchText) {
-      const res = await fetch(`https://newsapi.org/v2/top-headlines?q=${searchText}&apiKey=099148be22804e849a0c6fe022b7cf5e`);
-      const data = await res.json();
-      commit('setArticles', data.articles);
+      const res = await fetch(`https://newsapi.org/v2/top-headlines?q=${searchText}&apiKey=${process.env.VUE_APP_NEWS_API_KEY}`);
+      if (!res.ok) {
+        this.dialog = true;
+        const message = `An error occured: ${res.status}`;
+        throw new Error(message);
+      } else {
+        const data = await res.json();
+        commit('setArticles', data.articles);
+      }
     },
-  },
-  modules: {
   },
 });
